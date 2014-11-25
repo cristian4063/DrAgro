d6a90fe2e5672851828d033259383fe518954073
cristian4063 <varela0007@gmail.com>
2014-11-25 12:40:06 -0500

    validacion iOS

--- iOS/scripts/galeria.js
+++ iOS/scripts/galeria.js
@@ -14,6 +14,9 @@ $(document).ready(function() {
     if(applaunchCount) {
         changeLink();
     }
+    else {
+        abrirRedirect("Para acceder a esta sección por primera vez es necesario descargar información de internet, una vez tenga conectividad, por favor reinicie la aplicación.");
+    }
 
 });
 
@@ -212,4 +215,31 @@ function pintarImagenes(imagenes){
     idsFotos = [];
 
     $('.swipebox').swipebox();
+}
+
+function abrirRedirect(contenido)
+{
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var ancho=windowWidth-(windowWidth/10);
+    $('#content-alert').html('<p>'+contenido+'</p>');
+    $("#div-confirm").dialog({
+        modal: true,
+        draggable: false,
+        resizable: false,
+        title: 'Aviso',
+        minWidth:ancho,
+        my: "center",
+        at: "center",
+        of: window,
+        show: 'blind',
+        hide: 'blind',
+        dialogClass: 'prueba',
+        buttons: {
+            "Aceptar": function() {
+                $(this).dialog("close");
+                document.location.href="index.html";
+            }
+        }
+    });
 }
